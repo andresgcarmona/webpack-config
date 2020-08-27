4b6fd25ac5f37200c8d8e4887ef5b092eb64b090
Andres G. Carmona <ing.andres.carmona@gmail.com>
2020-08-26 21:39:59 -0500

    Completed webpage.js config file.

--- configs/parts/webpage.js
+++ configs/parts/webpage.js
@@ -2,7 +2,11 @@ const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin')
 
 exports.page = ({ title }) => ({
 	plugins: [
-	
-	]
+		new MiniHtmlWebpackPlugin({
+			context: {
+				title,
+			},
+		}),
+	],
 })
 
